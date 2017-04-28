63549144393f21daef5d102e0a6bc565b5ce255e
U-DESKTOP-RI82C4L\honte <ohernandezs1300@alumno.ipn.mx>
2017-04-27 21:50:35 -0500

    need some help

--- Ejemplo2/logic.js
+++ Ejemplo2/logic.js
@@ -1,7 +1,12 @@
-function getLabelSize(tH, rH)
+function toRelativeSize(tH, rH)
 {
    return 226-((200/tH)*rH);
 }
+function getTotalVolum()
+{
+    return  (Math.PI * Math.pow($("#userTotalR").val(),2) * $("#userTotalH").val()) / 3;
+}
+var flag;
 function updateLabels(pastH)
 {
     $("#svg_indRelH").attr("y1", pastH);
@@ -11,23 +16,34 @@ function updateLabels(pastH)
     if(pastH > 180)
         pastH = 180;
     $("#svg_txtRel").attr("transform", "matrix(0, 0.81, -1, 0, 225, "+ (pastH+5) + ")");
-       
+    
 }
 function transitionUpdate(pastH, nvaH) {
-    if (pastH==nvaH){ updateLabels(pastH); return; }
-
-    setTimeout(function () {
+    $("input").prop( "disabled", true );
+    //console.log(nvaH.toFixed(0) + " - " + pastH.toFixed(0));
+    if (pastH.toFixed(0) == nvaH.toFixed(0)){
+        $("input").prop( "disabled", false );
+        return;
+    }
+     setTimeout(function () {
         updateLabels(pastH);
-        if(nvaH>pastH )
+        flag = nvaH.toFixed(0)>pastH.toFixed(0);
+        console.log(nvaH.toFixed(2) + ">" + pastH.toFixed(2));
+        console.log(nvaH.toFixed(2) > pastH.toFixed(2) );
+        console.log(flag);
+        if(nvaH.toFixed(0)>pastH.toFixed(0) )
             transitionUpdate(++pastH, nvaH);
-        if(nvaH<pastH)
+        if(nvaH.toFixed(0)<pastH.toFixed(0))
             transitionUpdate(--pastH, nvaH);
     }, 10);
-}
+} 
 
 $(document).ready(function(){
-     var hInPx = 200;
-    var nvaH = getLabelSize($("#userTotalH").val(),($("#userRelH").val()));
+    var slider = document.getElementById("slider");
+    var relVol, totalVol;
+    var volPerMin;
+    var hInPx = 200;
+    var nvaH = toRelativeSize($("#userTotalH").val(),($("#userRelH").val()));
     var pastH = nvaH;
     var rInPx = 100;
     var svg_totalR = document.getElementById("svg_totalR");
@@ -46,74 +62,42 @@ $(document).ready(function(){
     svg_totalH.innerHTML = $("#userTotalH").val();
     svg_totalR.innerHTML = $("#userTotalR").val();
     //Eventos para actualizar etiquetas svg
+    updateLabels(pastH);
     transitionUpdate(pastH,nvaH);
     $("#userTotalR").on("change", function(){
+        totalVol = getTotalVolum();
         svg_totalR.innerHTML = $("#userTotalR").val();
     });
     $("#userTotalH").on("change", function(){
+        console.log("F");
+        totalVol = getTotalVolum();
         svg_totalH.innerHTML = $("#userTotalH").val();
         pastH = nvaH;
-        nvaH = getLabelSize($("#userTotalH").val(),($("#userRelH").val()));
+        nvaH = toRelativeSize($("#userTotalH").val(),($("#userRelH").val()));
         transitionUpdate(pastH,nvaH);
     });
     $("#userRelH").on("change", function(){
+        var inputH = $("#userRelH").val();
         pastH = nvaH;
-        nvaH = getLabelSize($("#userTotalH").val(),($("#userRelH").val()));
+        nvaH = toRelativeSize($("#userTotalH").val(),inputH);
         if(nvaH < 26 ) nvaH = 26;
         if(nvaH > 226 ) nvaH = 226;
+        $("#slider").val(inputH);
         $("#userRelH").val() <= $("#userTotalH").val() ? svg_relH.innerHTML = $("#userRelH").val() : svg_relH.innerHTML = $("#userTotalH").val();
         transitionUpdate(pastH,nvaH);
-
-        console.log(nvaH);
     });
-    /*
-    var slider = document.getElementById('slider');
-    //var userH = document.getElementById("userH").value;
-    var userR = document.getElementById("userR");
-    var nivelAgua = $("#nivelAgua");
     
-    noUiSlider.create(slider, {
-        start: 4.33,
-        connect: [true, false],
-        step: 0.02,
-        range: {
-            'min': 0,
-            'max': userH
-        }
-    });
-    slider.noUiSlider.on('update', function(values, handle) {
-
-        var rectangleH = values[handle];
-        var rectangleW = ((userTriangleS * (userTriangleH - values[handle])) / userTriangleH);
-        var rectangleHpercentage = rectangleH / userTriangleH * 100;
-
-        $('.verticall').css({'height': rectangleHpercentage + '%'});
-
-        $('.rectangle').css({
-            'width': rectangleW / userTriangleS * 100 + '%',
-            'height': rectangleHpercentage + '%'
-        });
+    $("#slider").on("change", function() {
+        var inputH = $("#slider").val();
+        pastH = nvaH;
+        nvaH = toRelativeSize($("#userTotalH").val(),inputH);
+        if(nvaH < 26 ) nvaH = 26;
+        if(nvaH > 226 ) nvaH = 226;
+        $("#userRelH").val(inputH);
+        $("#userRelH").val() <= $("#userTotalH").val() ? svg_relH.innerHTML = $("#userRelH").val() : svg_relH.innerHTML = $("#userTotalH").val();
+        transitionUpdate(pastH,nvaH);
         
-        inputArea.value = (rectangleH * rectangleW).toFixed(2);
-        inputRectangleH.value = values[handle];
     });
 
-    inputRectangleH.addEventListener('change', function() {
-        slider.noUiSlider.set(this.value);
-    });
-
-    inputUserTriangleS.addEventListener('change', function() {
-
-        userTriangleS = inputUserTriangleS.value;
-        userTriangleH = (userTriangleS / 2) * sqrt3;
-
-
-        slider.noUiSlider.updateOptions({
-            range: {
-                'min': 0,
-                'max': userTriangleH
-            }
-        });
-    });
-   $("#nivelAgua").attr("offset", "50%")*/
+    
 });
