c44fe9e18a4c17b433f43fce704f910c1c4eed46
U-DESKTOP-RI82C4L\honte <ohernandezs1300@alumno.ipn.mx>
2017-05-05 20:18:07 -0500

    some fixes :v

--- Ejemplo2/logic.js
+++ Ejemplo2/logic.js
@@ -1,35 +1,4 @@
-/*
-function toRelativeSize(tH, rH)
-{
-   return 226-((200/tH)*rH);
-}
-function getTotalVolum()
-{
-    return  (Math.PI * Math.pow($("#userTotalR").val(),2) * $("#userTotalH").val()) / 3;
-}
-var flag;
-function updateLabels(pastH)
-{
-    $("#svg_indRelH").attr("y1", pastH);
-    $("#efectoAgua").attr("offset",(((pastH)/200)*100)-15 + "%");
-    $("#nivelAgua").attr("offset",(((pastH)/200)*100)-10 + "%");
-    $("#svg_topH").attr("points", " 222.66 " + pastH + " 202.33 " + pastH);
-    if(pastH > 180)
-        pastH = 180;
-    $("#svg_txtRel").attr("transform", "matrix(0, 0.81, -1, 0, 225, "+ (pastH+5) + ")");
-    
-}
-function transitionUpdate(pastH, nvaH) {
-    if (pastH.toFixed(0) == nvaH.toFixed(0)) return;
-     setTimeout(function () {
-        updateLabels(pastH);
-        if(nvaH.toFixed(0)>pastH.toFixed(0) )
-            transitionUpdate(++pastH, nvaH);
-        if(nvaH.toFixed(0)<pastH.toFixed(0))
-            transitionUpdate(--pastH, nvaH);
-    }, 10);
-} 
-*/
+
 function getY(perimetro, x)
 {
     var res = -perimetro + ((x*Math.PI).toFixed(1)/2) + parseFloat(x);
@@ -56,13 +25,16 @@ function onChangePer(perimetro, x)
     onChangeX(perimetro, x);
     onChangeArea(x, getY(perimetro, x));
 }
+function responsive(svg)
+{
+    svg.width($(window).width() * 0.95);
+    svg.height($(window).height()*0.50);
+}
 $(document).ready(function(){
     //Resize
-    $("#mySVG").width($(window).width());
-    $("#mySVG").height($(window).height()*0.50);
+    responsive($("#mySVG"));
     $(window).resize(function(){
-        $("#mySVG").width($(window).width());
-        $("#mySVG").height($(window).height()*0.50);
+        responsive($("#mySVG"));
         console.log($(window).width() + "x" + $(window).height());
     });
     //Vars
@@ -77,13 +49,11 @@ $(document).ready(function(){
     {
         onChangeX(perimetro, x);
         x = $("#mySlider_x").val();
-        
     });
     $("#mySlider_per").on("input", function()
     {
         onChangePer(perimetro, x);
         perimetro = $("#mySlider_per").val();
-        
     });
     
 });
